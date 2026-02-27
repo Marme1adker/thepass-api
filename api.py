1042975f236d6474cbf3a9661c47fa8c8271cda1
Marme1adka <116520778+Marme1adker@users.noreply.github.com>
2026-02-28 00:13:14 +0200

    Update api.py

--- api.py
+++ api.py
@@ -192,7 +192,7 @@ app = FastAPI(title="The Pass API", docs_url=None, redoc_url=None)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
